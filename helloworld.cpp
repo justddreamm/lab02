cb3202b5510c282e328ceb1fff200542075b06e6
justddreamm <79227040+justddreamm@users.noreply.github.com>
2021-03-10 22:44:57 +0300

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,5 +6,5 @@ int main()
 std::string username;
 std::cout<<"Enter your name: "; // asking for name
 std::cin>>username; // entering name here
-std::cout<<"Hello World from "<<username<<std::endl; //printing the data
+std::cout<<"Hello World from "<<username<<std::endl; //printing the data EDITED
 }
